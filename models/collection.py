fc5126265bff0c6b0e32984ef06c4a3b6cc269dd
blackbelts-egypt <mabdelwahed120468@gmail.com>
2020-07-07 10:52:40 +0200

    test

--- models/collection.py
+++ models/collection.py
@@ -8,11 +8,11 @@ class AropeCollection(models.Model):
     _name='collection.arope'
     policy=fields.Many2one('policy.arope',string='Policy')
     customer=fields.Many2one(related='policy.customer' ,store=True ,string='Customer')
-    collect_date=fields.Date(string='Collected Date')
+    collect_date=fields.Date(string='Collected Date',related='policy.issue_date',store=True)
     net_premium=fields.Float(string='Net Premium',related='policy.net_premium',store=True)
     gross_premium=fields.Float(string='Gross Premium',related='policy.gross_premium',store=True)
     broker=fields.Many2one(related='policy.broker',string='Broker')
     state = fields.Selection([('outstanding', 'Outstanding'),
                                ('paid', 'Paid'),
                               ('canceled', 'Canceled')],
-                             'State', required=True, default='paid')
+                             'State', required=True, default='outstanding')
