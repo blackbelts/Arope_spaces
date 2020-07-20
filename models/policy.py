56f6ea76f3c1135a59667c3009eeb8322b6b8ac4
blackbelts-egypt <mabdelwahed120468@gmail.com>
2020-07-20 15:55:00 +0200

    test

--- models/policy.py
+++ models/policy.py
@@ -49,10 +49,12 @@ class AropePolicy(models.Model):
     index=fields.Date()
     collection_ids=fields.One2many('collection.arope','policy','Collections')
     risk_ids=fields.One2many('policy.risk','policy_risk_id',string='Risks')
-    check_item = fields.Char(compute='_compute_insured_policy')
+    check_item = fields.Char(compute='_compute_insured_policy',store=True)
     ins_type = fields.Selection([('Individual', 'Individual'),
                                  ('Group', 'Group'), ],
                                 'I&G', track_visibility='onchange', copy=True, default='Individual', required=True)
+
+    @api.model
     @api.depends('line_of_bussines', 'ins_type')
     def _compute_insured_policy(self):
         if self.ins_type == 'Group':
