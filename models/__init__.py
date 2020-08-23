6f755fc80121ba22a0b854f6bf8351adbde997f6
blackbelts-egypt <mabdelwahed120468@gmail.com>
2020-08-23 18:34:50 +0200

    test

--- models/__init__.py
+++ models/__init__.py
@@ -4,7 +4,7 @@ from . import setup
 from . import  policy
 from . import  partner
 from . import collection
-# from . import policy_risk
+from . import policy_risk
 from . import get_a_qoute
 from . import price_setup
 from . import team_target
