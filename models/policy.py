f9a99678e6632555d8594797b5e49c398e8c2895
blackbelts-egypt <mabdelwahed120468@gmail.com>
2020-08-27 15:40:49 +0200

    test

--- models/policy.py
+++ models/policy.py
@@ -13,7 +13,7 @@ class AropePolicy(models.Model):
     term = fields.Char()
     total_sum_insured = fields.Float(digits=(12, 2), string='Total Sum Insured')
     net_premium= fields.Float(string="Net Premium", copy=True)
-    gross_premium= fields.Float(string="Gross Premium", copy=True)
+    c= fields.Float(string="Gross Premium", copy=True)
     currency_id = fields.Char("Currency", copy=True)
     policy_status = fields.Char(string='Status' ,copy=False)
     endorsement_no = fields.Char(string="Endorsement No.")
