20847843ec1396cf0fc29ec55a0414cd425783bf
blackbelts-egypt <mabdelwahed120468@gmail.com>
2020-09-28 10:28:21 +0200

    test

--- wizard/users.py
+++ wizard/users.py
@@ -25,6 +25,6 @@ class AgentUsersWizard(models.TransientModel):
         user=self.env['res.users'].create(
             {'name': self.name, 'login':self.card_id, 'password':self.password, 'agent_code': self.agent_code,
              'card_id': self.card_id,
-             'is_broker': True, 'groups_id': [
+              'groups_id': [
                 self.env['res.groups'].search([('name', '=', 'Broker')]).id]})
         person.write({'related_user':user.id})
