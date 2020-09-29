161d4c8d03e532c0c10e17df465de2f1689efe02
blackbelts-egypt <mabdelwahed120468@gmail.com>
2020-09-29 15:23:39 +0200

    update

--- models/hrms_dashboard.py
+++ models/hrms_dashboard.py
@@ -440,7 +440,7 @@ class Brokers(models.Model):
                                                       ('state_for', '=', 'broker')]):
             status.append({"name": record.state, "message": record.message})
         for offer in self.env['insurance.quotation'].search([('id', '=', id)]).offer_ids:
-            offers.append({"file_id": offer.id, "type": offer.dict(offer._fields['type'].selection).get(offer.type),
+            offers.append({"file_id": offer.id, "type": dict(offer._fields['type'].selection).get(offer.type),
                            "state": offer.offer_state})
         return {'status': status, 'app': rec, 'offers': offers}
 
