dc3bd4e0a1756ce9a7f9172d88cc504c7cf6e96b
blackbelts-egypt <mabdelwahed120468@gmail.com>
2020-10-01 17:12:12 +0200

    update

--- models/quotion_service.py
+++ models/quotion_service.py
@@ -214,8 +214,8 @@ class QuotationService(models.Model):
         self.write({"lob": 1})
 
     def travel(self):
-        id = self.env['insurance.line.business'].search([('line_of_business', '=', 'Travel')]).id
-        self.write({"lob": id})
+        # id = self.env['insurance.line.business'].search([('line_of_business', '=', 'Travel')]).id
+        self.write({"lob": 6})
 
 class Members(models.Model):
     _name = 'members'
