4d2e047062ce69eedbbd854ea3d71cafd512a835
blackbelts-egypt <mabdelwahed120468@gmail.com>
2020-10-07 11:28:36 +0200

    update

--- models/insurance_application.py
+++ models/insurance_application.py
@@ -499,7 +499,7 @@ class Quotation(models.Model):
             number = self.env['ir.sequence'].next_by_code('survey')
             currentYear = datetime.today().strftime("%Y")
             currentMonth = datetime.today().strftime("%m")
-            
+
             id = self.env['survey.report'].create(
                 {"name": "Survey"+ '/' + currentYear[2:4] + '/' + currentMonth +
                                        '/' + number ,
