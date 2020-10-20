bbb846bd8a0ec3347876f15b6e01d5125a39eba4
blackbelts-egypt <mabdelwahed120468@gmail.com>
2020-10-20 11:04:07 +0200

    test

--- __manifest__.py
+++ __manifest__.py
@@ -5,7 +5,7 @@
     'description': """Spaces """,
     'author': "Black Belts Egypt",
     'website': "www.blackbelts-egypt.com",
-    'category': 'arope',
+    'category': 'space',
     'version': '0.1',
     'license': 'AGPL-3',
     # any module necessary for this one to work correctly
