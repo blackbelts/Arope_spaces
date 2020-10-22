de9511561c3e6bb4fd3cc443d1b1c075da2d24d7
blackbelts-egypt <mabdelwahed120468@gmail.com>
2020-10-22 14:45:09 +0200

    test

--- models/policy_request.py
+++ models/policy_request.py
@@ -37,7 +37,7 @@ class AropePolicyRequests(models.Model):
     @api.onchange('policy', 'policy_seq')
     def get_policy(self):
         if self.policy and self.policy_seq:
-            pol = self.env['policy.arope'].search([('product', '=', self.policy_seq.product_name)
+            pol = self.env['policy.arope'].search([
                                                    ], limit=1)
             # self.customer = str(pol.pin)
             # self.agent_code = str(pol.pin)
