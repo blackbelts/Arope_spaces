0b3fd0bb80ead2b74d502be48e876629a5f63689
blackbelts-egypt <mabdelwahed120468@gmail.com>
2020-10-22 16:59:33 +0200

    update

--- models/state.py
+++ models/state.py
@@ -40,7 +40,7 @@ class StateSetup(models.Model):
     #                               ('surveyor', 'Surveyor'),('underwriter', 'Underwriter')], string='State For')
     message = fields.Text('Message')
 
-    @api.onchange('status', 'claim_status', 'survey_status','non_motor_claim_status')
+    @api.onchange('status', 'claim_status', 'survey_status', 'non_motor_claim_status')
     def compute_status(self):
         if self.status:
             self.state = dict(self._fields['status'].selection).get(self.status)
