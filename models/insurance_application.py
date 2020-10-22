b781b6f2bacb25c0c9c3ba80843737ec56119345
blackbelts-egypt <mabdelwahed120468@gmail.com>
2020-10-22 11:31:58 +0200

    update

--- models/insurance_application.py
+++ models/insurance_application.py
@@ -360,13 +360,12 @@ class Quotation(models.Model):
 
     @api.onchange('persons')
     def get_application_form(self):
-        print('12345')
-        # all_persons = []
-        # for rec in self.persons:
-        #     all_persons.append(rec)
-        #     rec.write({'download_files':
-        #         [(0,0,{'name': 'Questionnaire', 'res_name': 'questionnaire','type': 'binary','datas': self.product_id.questionnaire_file[0].datas})],
-        #                'insured': 'Insurer'+ str(len(all_persons))})
+        all_persons = []
+        for rec in self.persons:
+            all_persons.append(rec)
+            rec.write({'download_files':
+                [(0,0,{'name': 'Questionnaire', 'res_name': 'questionnaire','type': 'binary','datas': self.product_id.questionnaire_file[0].datas})],
+                       })
 
 
     @api.onchange('dob','product')
