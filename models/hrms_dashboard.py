b811ce8223e7058be0adaf90228732ea1cf8b631
blackbelts-egypt <mabdelwahed120468@gmail.com>
2020-10-27 11:32:03 +0200

    test

--- models/hrms_dashboard.py
+++ models/hrms_dashboard.py
@@ -711,6 +711,17 @@ class Brokers(models.Model):
         # self.env['final.offer'].search([('id', '=', id)])[0].write({'type': ''})
         # return True
 
+    @api.model
+    def current_user(self):
+        context = self.env.context
+        record = self.env[context['active_model']].browse(
+            context['active_id'])
+        if context['active_model'] == 'persons':
+            return self.env['res.users'].search([('card_id','=',record.card_id)]).id
+        else:
+            return self.env.user.id
+
+
 
 
 
