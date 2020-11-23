838ecd0ff7cdccb93aad7aa0916abed61e3a18e2
blackbelts-egypt <mabdelwahed120468@gmail.com>
2020-11-23 16:53:58 +0200

    update

--- models/quotion_service.py
+++ models/quotion_service.py
@@ -226,16 +226,16 @@ class QuotationService(models.Model):
 
 
     def motor(self):
-        self.lob = self.env[''].search([('line_of_business', '=', 'Motor')]).id
+        self.lob = self.env['insurance.line.business'].search([('line_of_business', '=', 'Motor')]).id
         self.get_lob_name()
 
     def medical(self):
-        self.lob = self.env[''].search([('line_of_business', '=', 'Medical')]).id
+        self.lob = self.env['insurance.line.business'].search([('line_of_business', '=', 'Medical')]).id
         self.get_lob_name()
 
     def travel(self):
         # id = self.env['insurance.line.business'].search([('line_of_business', '=', 'Travel')]).id
-        self.lob = self.env[''].search([('line_of_business', '=', 'Travel')]).id
+        self.lob = self.env['insurance.line.business'].search([('line_of_business', '=', 'Travel')]).id
         self.get_lob_name()
 
     def create_app(self):
