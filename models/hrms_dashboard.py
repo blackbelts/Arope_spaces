7a4bbe87f5a678706f4afa1ce3cb3cd30a9124ce
blackbelts-egypt <mabdelwahed120468@gmail.com>
2020-12-07 23:34:10 +0200

    update

--- models/hrms_dashboard.py
+++ models/hrms_dashboard.py
@@ -711,8 +711,7 @@ class Brokers(models.Model):
     def accept_offer(self,id):
         offer = self.env['final.offer'].search([('id', '=', id)])
         for rec in offer:
-            if rec.type == 'final':
-                rec.application_id.change_offer()
+            self.env['insurance.quotation'].search([('id', '=', rec.application_id.id)]).change_offer()
             rec.write({'offer_state': 'accepted'})
         return True
 
