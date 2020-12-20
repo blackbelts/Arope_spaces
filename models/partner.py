cf0438957a20da48e008abfaa0379cfdcb2fb904
blackbelts-egypt <mabdelwahed120468@gmail.com>
2020-12-20 13:02:22 +0200

    test

--- models/partner.py
+++ models/partner.py
@@ -5,11 +5,10 @@ from odoo.exceptions import ValidationError
 
 
 class InheritBrokers(models.Model):
-    _inherit = 'persons'
+    _inherit = 'res.users'
 
 
     def view_dashboard(self):
-        print('kkkkkkkkkkkkkkkkkk')
         # self.env['arope.broker'].current_user(self.env.context)
         # form = self.env.ref('Arope_spaces.brokers_user_wizard')
         # self.is_user = True
