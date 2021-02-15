275a29481ac8cf701b427c2846e362bd571c9ff0
blackbelts-egypt <mabdelwahed120468@gmail.com>
2021-02-15 23:49:58 +0200

    update

--- models/sign_up.py
+++ models/sign_up.py
@@ -7,7 +7,7 @@ from odoo.http import request
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
-    # pin = fields.Char(string='PIN')
+    x_pin = fields.Char(string='PIN')
 
     # @api.model
     # def _signup_create_user(self, values):
