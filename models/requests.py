369f071001b4e48726286b6e5be1ed26409aa814
blackbelts-egypt <mabdelwahed120468@gmail.com>
2021-03-30 02:29:43 +0200

    update

--- models/requests.py
+++ models/requests.py
@@ -57,7 +57,7 @@ class CrmLeads(models.Model):
     customer_name = fields.Char('Customer Name')
     phone = fields.Char('Customer Mobile')
     email = fields.Char('Customer Email')
-    clickable = fields.Boolean('_get_group_security', 'is clickable' , store=True)
+    clickable = fields.Boolean(compute='_get_group_security', 'is clickable' , store=True)
     application_number = fields.Char(string='Application Number', copy=False, index=True)
     application_date = fields.Date('Application Date', default=datetime.today(), readonly=True)
     policy_number = fields.Char('Policy Num')
@@ -179,9 +179,9 @@ class CrmLeads(models.Model):
     def _get_group_security(self):
         for rec in self:
             if self.env.user.has_group('Arope_spaces.broker_space_group'):
-                return False
+                rec.clickable =  False
             else:
-                return False
+                rec.clickable =  False
 
 
     @api.onchange('offer')
